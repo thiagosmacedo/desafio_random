b61be0234aa64880cbd2053f60134738eeb76979
thiagosmacedo <thiago.gigacomp@gmail.com>
2024-04-29 14:21:23 -0300

    MELHORA

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ function generateNumbers() {
   const min = Math.ceil(document.querySelector(".minimo").value)
   const max = Math.floor(document.querySelector(".maximo").value)
 
+
+
   const result = Math.floor(Math.random() * (max - min + 1)) + min;
   const alerta = document.getElementById("winner")
 
-  alerta.innerHTML = "Número " + result 
+
+  if (max > min) {
+    alerta.innerHTML = "Número " + result
+
+  } else {
+    alert("O número tem que ser MAIOR que o minimo")
+  }
+
+
 
   //console.log(result);
 }
